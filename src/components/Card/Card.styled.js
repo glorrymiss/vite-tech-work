431ef3f888d573b08c80218024859e88d9af697e
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-05-25 17:15:33 +0200

    add text bottom

--- src/components/Card/Card.styled.js
+++ src/components/Card/Card.styled.js
@@ -69,4 +69,14 @@ export const Button = styled.button`
   box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
   border-radius: 10.3108px;
   padding: 14px 56px;
+
+  font-family: "Montserrat", sans-serif;
+
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 1.22;
+
+  text-transform: uppercase;
+
+  color: #373737;
 `;
