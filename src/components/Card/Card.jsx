791391343230d32ebddec992d2d4fd5fc9021fa4
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-05-25 10:58:33 +0200

    add insite

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+export const Card = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <div></div>
+      <img src="" alt="" />
+      <div>
+        <p></p>
+        <p></p>
+      </div>
+      <button></button>
+    </div>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome! Nice to meet you! Have a nice time!</h2>
+    </div>
+  );
+};
+
+export default Home;
