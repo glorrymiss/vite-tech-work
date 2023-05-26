83fa33a97870fa90e147510150848db8baceb469
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-05-26 12:15:32 +0200

    add

--- src/components/BtnLoadMore/BtnLoadMore.styled.js
+++ src/components/BtnLoadMore/BtnLoadMore.styled.js
@@ -2,9 +2,15 @@ import styled from "@emotion/styled";
 
 export const BtnLoad = styled.button`
   width: 200px;
-  background-color: rgb(0, 102, 0);
+  background: linear-gradient(
+    114.99deg,
+    #471ca9 -0.99%,
+    #5736a3 54.28%,
+    #4b2a99 78.99%
+  );
+  box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
   color: white;
-  margin: 0 35% 80px;
+  margin: 0 45% 80px;
 
   padding: 8px 26px;
   font-weight: 600;
@@ -12,6 +18,6 @@ export const BtnLoad = styled.button`
   border-radius: 6px;
   border: transparent;
   &:hover {
-    background-color: rgb(0, 153, 0);
+    color: orangered;
   }
 `;
