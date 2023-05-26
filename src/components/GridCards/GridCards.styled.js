9402418651a7336335fc7c0bcb20c355333c5c56
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-05-26 12:13:04 +0200

    update list

--- src/components/GridCards/GridCards.styled.js
+++ src/components/GridCards/GridCards.styled.js
@@ -3,6 +3,8 @@ import styled from "@emotion/styled";
 export const List = styled.ul`
   display: flex;
   flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
 
   gap: 30px;
   margin: 30px 0 70px;
